4d77e03fe4e565ad8c8e96d22eb863dd054c6816
inspiron <happyvivus@gmail.com>
2017-10-08 12:01:18 +0200

    interfaces

---
+++ LangageProd_TP1/src/Files/Interface/File.java
@@ -0,0 +1,54 @@
+package Files.Interface;
+
+public interface File<T> {
+
+    /**
+     * Renvoie le premier element de la file
+     *
+     * @return    le premier element de la file
+     */
+    T getTete();
+
+    /**
+     * Revoie le dernier element de la file
+     *
+     * @return dernier élément
+     */
+    T getQueue();
+
+    /**
+     * Retourne tout les éléments de la file de manière ordonnée
+     *
+     * @return La liste des éléments contenus dans la file
+     */
+    Object getElements();
+
+    /**
+     * Elève le dernier élement de la file
+     *
+     * @return un objet représentant la tete de la file pour ne pas perdre d'information
+     */
+    Object enleverTete();
+
+    /**
+     * Teste si un élément est/ou non dans la file
+     *
+     * @param element
+     * @return Vrai si l'élément est présent dans la file
+     */
+    boolean contient(T element);
+
+    /**
+     * Revoie le nombre d'éléments présents dans la fille
+     *
+     * @return taille
+     */
+    int taille();
+
+    /**
+     * Teste si la file est vide ou non
+     *
+     * @return Vrai si la file est vide
+     */
+    boolean estVide();
+}
---
+++ LangageProd_TP1/src/Files/Interface/FileMutable.java
@@ -0,0 +1,76 @@
+package Files.Interface;
+
+import java.util.EmptyStackException;
+import java.util.NoSuchElementException;
+
+public interface FileMutable<T> extends File<T> {
+
+    /**
+     * Renvoie le premier element de la file
+     *
+     * @return    le premier element de la file
+     */
+    T getTete() throws NoSuchElementException;
+
+    /**
+     * Revoie le dernier element de la file
+     *
+     * @return dernier élément
+     */
+    T getQueue() throws NoSuchElementException;
+
+    /**
+     * Retourne tout les éléments de la file de manière ordonnée
+     * Le type de retour dois être adapté au type d'implémentation
+     *
+     * @return La liste des éléments contenus dans la file
+     */
+    Object getElements();
+
+    /**
+     * Ajoute un element du meme type que les elements de la file en bout de file
+     * La modification se fait sur l'objet même
+     *
+     * @param  element  Un element de type T
+     */
+    void ajouterQueue(T element);
+
+    /**
+     * Renvoie le premier element de la file et le supprime apres de la file
+     * La modification se fait sur l'objet même
+     * Preconditions: La file ne dois pas être vide.
+     *
+     * @return  Un element de type T
+     */
+    T enleverTete() throws EmptyStackException;
+
+    /**
+     * Teste si un élément est/ou non dans la file
+     *
+     * @param element
+     * @return Vrai si l'élément est présent dans la file
+     */
+    boolean contient(T element);
+
+    /**
+     * Revoie le nombre d'éléments présents dans la fille
+     *
+     * @return taille
+     */
+    int taille();
+
+    /**
+     * Concatène a la queue une autre file f en conservant l'ordre de ses éléments
+     * Pre Condition: les éléments de l'autre file sont également de type T
+     *
+     * @param f une autre File
+     */
+    void concatener(File<T> f);
+
+    /**
+     * Teste si la file est vide ou non
+     *
+     * @return Vrai si la file est vide
+     */
+    boolean estVide();
+}
