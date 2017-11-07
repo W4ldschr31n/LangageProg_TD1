904dfe63eb72da85cc1890d59a1cb550e5633f23
inspiron <happyvivus@gmail.com>
2017-11-07 20:58:46 +0100

    Interfqce file (et aussi arreter de pusher vos settings d'ide les enfants )

--- LangageProd_TP1/src/tp/v2/File.java
+++ LangageProd_TP1/src/tp/v2/File.java
@@ -2,37 +2,68 @@ package tp.v2;
 
 public interface File<E> extends Iterable<E> {
 
-	/*
-	 * Accesseurs
+	/**
+	 * Accesseur premier élément
+	 * @return premier élément de la file
 	 */
 	E premier();
+	/**
+	 * Accesseur suivants
+	 * @return file des éléments suivants
+	 */
 	File<E> suivants();
-	
+
+	/**
+	 * Asserte si la file est vide
+	 * @return true si la file est vide
+	 */
 	default boolean estVide() {
 		return this.taille() == 0;
 	}
 	int taille();
-	
-	/*
-	 * Fabriques
+
+	/**
+	 * Fabrique
+	 * @return une instance de File
 	 */
 	File<E> creer();
-	
-	/*
-	 * Services
+
+	/**
+	 * Ajoute un élément a la queue de la file
+	 * @param dernierDansFile
+	 * @return la file qui résulte de l'ajout
 	 */
 	File<E> ajout(E dernierDansFile);
+
+	/**
+	 * Supprime l'élément de tête
+	 * @return la file qui résulte du retrait
+	 */
 	File<E> retrait();
+
+	/**
+	 * Ajoute en queue une file d'éléments
+	 * @param secondeFile
+	 * @return la file résultant de l'ajout
+	 */
 	File<E> ajout(File<E> secondeFile);
 	
 	default String representation() {
-		// TODO 
-		return "";
+		String representation = "[/]";
+		if(!estVide()){
+			representation += "["+premier().toString()+"]"+suivants().representation();
+		}
+		return representation;
 	}
 
+	/**
+	 * Compare l'égalitée de deux file
+	 * Pré cond: Les éléments de type E doivent définir une méthode equals  qui ait du sens
+	 * @param file
+	 * @return
+	 */
 	default boolean estEgal(File<E> file){
-		// TODO
-		return true;
+		return premier().equals(file.premier()) && suivants().estEgal(file.suivants());
 	}
 	
 	
