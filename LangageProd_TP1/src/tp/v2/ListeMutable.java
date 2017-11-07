5c0709230685c1b3bea2218a26f96592a6017b3c
looiuuytr <sizeotomai@msn.com>
2017-11-07 20:16:43 +0100

    Code methodes cons et miroir

--- LangageProd_TP1/src/tp/v2/ListeMutable.java
+++ LangageProd_TP1/src/tp/v2/ListeMutable.java
@@ -1,5 +1,7 @@
 	package tp.v2;
 
+import java.util.LinkedList;
+
 public interface ListeMutable<E> extends Liste<E>{
 
 	/*
@@ -21,13 +23,29 @@ public interface ListeMutable<E> extends Liste<E>{
 	 * Services
 	 */
 	default ListeMutable<E> miroir(){
-		// TODO
-		return null;
+		LinkedList<E> liste = new LinkedList<>();
+		for (E element: this){
+			liste.add(element);
+		}
+		vide();// on vide la ListeMutable
+		for (int i=liste.size()-1;i>=0;i--){
+			cons(liste.get(i), this);// on ajoute en tete les elements en partant de la fin de la liste
+		}
+		return this;
+		
 	}
 
 	public static <E> ListeMutable<E> cons(E t, ListeMutable<E> r){
 		return new ListeMutable<E>() {
-			// TODO Définir les méthodes utiles.			
+			@Override
+			public void changerTete(E tete) {
+				ListeMutable.super.changerTete(t);
+			}
+			@Override
+			public void changerReste(ListeMutable<E> r){
+				ListeMutable.super.changerReste(r);
+			}
+			
 		};
 	}
 	
