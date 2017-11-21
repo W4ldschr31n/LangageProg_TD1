a13510a1c65105c525afda39d840ef19e6e59632
Le-Leah <happyvivus@gmail.com>
2017-11-21 22:35:52 +0100

    test mutables

--- LangageProd_TP1/src/filRouge/v5/tests/etatFileMutableTest.java
+++ LangageProd_TP1/src/filRouge/v5/tests/etatFileMutableTest.java
@@ -7,15 +7,15 @@ public class etatFileMutableTest {
         EtatFileMutable<EtatFileMutable,String> etat0 = EtatFileMutable.vide();
 
         System.out.println("taille " +etat0.taille());
-        System.out.println("est vise: "+etat0.estVide());
+        System.out.println("estVide true ? = "+etat0.estVide());
         etat0 = EtatFileMutable.cons("un",etat0);
-        System.out.println(etat0.representer());
+        System.out.println("File [un] = "+etat0.representer());
         etat0 = EtatFileMutable.cons("deux",etat0);
         etat0 = EtatFileMutable.cons("trois",etat0);
-        System.out.println(etat0.representer());
-        System.out.println(etat0.taille());
-        System.out.println(etat0.premier());
-        System.out.println(etat0.suivants().representer());
+        System.out.println("File [un,deux,trois]="+etat0.representer());
+        System.out.println("taille  3 ? = "+etat0.taille());
+        System.out.println("tete  un ? = "+etat0.premier());
+        System.out.println("Suivants [deux,trois]="+etat0.suivants().representer());
     }
 
 }
